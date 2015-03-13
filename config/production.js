d4a0401d8e96b2a40d8827aa50b3338ae524a2f9
NatyCana <naty@mbpdinacanonica.einet.ad.eivd.ch>
2015-03-13 14:01:30 +0100

    App

---
+++ config/production.js
@@ -0,0 +1,5 @@
+{
+	"apiUrl": "https://polar-brook-7624.herokuapp.com/api",
+	"mapboxMapId": "natycana.kajond19",
+	"mapboxAccessToken": "pk.eyJ1IjoibmF0eWNhbmEiLCJhIjoiM1hLX2J1byJ9.yJwiq2hLXPFybDvnOVA0pw",
+}
