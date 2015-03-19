d8c94c39e0be2ba28a2be50dd9171973c6a6c576
may0u <maya.jeanmonod@gmail.com>
2015-03-19 13:09:11 +0100

    back button ok

--- www/js/constants.js
+++ www/js/constants.js
@@ -1,7 +1,5 @@
 angular.module('citizen-engagement.constants', [])
-
   	.constant('apiUrl', 'http://localhost:8100/api-proxy')
 	.constant("mapboxMapId", "may0u.kajp3428")
 	.constant("mapboxAccessToken", "pk.eyJ1IjoibWF5MHUiLCJhIjoiejdhbFJaQSJ9.pLvm9eE9f02HkFvFOPPcCQ")
-
 ;
