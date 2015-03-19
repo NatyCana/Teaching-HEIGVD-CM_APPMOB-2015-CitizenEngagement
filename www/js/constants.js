ef35c5f69ba4de7105423c70da143ea7f54d9d33
NatyCana <naty@mbpdinacanonica.einet.ad.eivd.ch>
2015-03-19 11:57:36 +0100

    modifications constants

--- www/js/constants.js
+++ www/js/constants.js
@@ -1,9 +1,5 @@
 angular.module('citizen-engagement.constants', [])
 
-  	.constant('apiUrl', 'http://10.192.112.23:8100/api-proxy')
-	.constant("mapboxMapId", "natycana.kajond19")
-	.constant("mapboxAccessToken", "pk.eyJ1IjoibmF0eWNhbmEiLCJhIjoiM1hLX2J1byJ9.yJwiq2hLXPFybDvnOVA0pw")
-
   	.constant('apiUrl', 'http://localhost:8100/api-proxy')
 	.constant("mapboxMapId", "may0u.kajp3428")
 	.constant("mapboxAccessToken", "pk.eyJ1IjoibWF5MHUiLCJhIjoiejdhbFJaQSJ9.pLvm9eE9f02HkFvFOPPcCQ")
