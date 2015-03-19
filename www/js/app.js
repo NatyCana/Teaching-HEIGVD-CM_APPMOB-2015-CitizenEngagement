6eb5b179d23c3e576618d463f7a460f25dc3df96
may0u <maya.jeanmonod@gmail.com>
2015-03-19 16:36:18 +0100

    mapmarker ok

--- www/js/app.js
+++ www/js/app.js
@@ -3,12 +3,20 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('citizen-engagement', ['ionic', 'citizen-engagement.auth', 'citizen-engagement.constants', "leaflet-directive", "geolocation", 'ui.router'])
+angular.module('citizen-engagement',
+        ['ionic',
+            'citizen-engagement.auth',
+            'citizen-engagement.constants',
+            'leaflet-directive',
+            'geolocation',
+            'ui.router',
+            'citizen-engagement.services'
+        ])
 
         .run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
+// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+// for form inputs)
         if (window.cordova && window.cordova.plugins.Keyboard) {
             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         }
@@ -27,7 +35,7 @@ angular.module('citizen-engagement', ['ionic', 'citizen-engagement.auth', 'citiz
         /*///////////////SERVICE//PHOTO//////////*/
         /*///////////////////////////////////////*/
 
-        .factory("CameraServie", function($q) {
+        .factory("CameraService", function($q) {
     return{
         getPicture: function(options) {
             var deferred = $q.defer();
@@ -41,14 +49,17 @@ angular.module('citizen-engagement', ['ionic', 'citizen-engagement.auth', 'citiz
     }
 
 })
+
+        /*///////////////////////////////////////*/
+        /*///////////////$stateProvider//////////*/
+        /*///////////////////////////////////////*/
+
         .config(function($stateProvider, $urlRouterProvider) {
 
-
-
-    // Ionic uses AngularUI Router which uses the concept of states
-    // Learn more here: https://github.com/angular-ui/ui-router
-    // Set up the various states which the app can be in.
-    // Each state's controller can be found in controllers.js
+// Ionic uses AngularUI Router which uses the concept of states
+// Learn more here: https://github.com/angular-ui/ui-router
+// Set up the various states which the app can be in.
+// Each state's controller can be found in controllers.js
     $stateProvider
 
             // This is the abstract state for the tabs directive.
@@ -61,13 +72,13 @@ angular.module('citizen-engagement', ['ionic', 'citizen-engagement.auth', 'citiz
             // The three next states are for each of the three tabs.
             // The state names start with "tab.", indicating that they are children of the "tab" state.
             .state('tab.newIssue', {
-        // The URL (here "/newIssue") is used only internally with Ionic; you never see it displayed anywhere.
-        // In an Angular website, it would be the URL you need to go to with your browser to enter this state.
+// The URL (here "/newIssue") is used only internally with Ionic; you never see it displayed anywhere.
+// In an Angular website, it would be the URL you need to go to with your browser to enter this state.
         url: '/newIssue',
         views: {
-            // The "tab-newIssue" view corresponds to the <ion-nav-view name="tab-newIssue"> directive used in the tabs.html template.
+// The "tab-newIssue" view corresponds to the <ion-nav-view name="tab-newIssue"> directive used in the tabs.html template.
             'tab-newIssue': {
-                // This defines the template that will be inserted into the directive.
+// This defines the template that will be inserted into the directive.
                 templateUrl: 'templates/newIssue.html',
                 controller: "NewIssue"
             }
@@ -89,7 +100,7 @@ angular.module('citizen-engagement', ['ionic', 'citizen-engagement.auth', 'citiz
         views: {
             'tab-issueList': {
                 templateUrl: 'templates/issueList.html',
-                controller: "GetIssues"
+                controller: "IssueList"
             }
         }
     })
@@ -103,12 +114,12 @@ angular.module('citizen-engagement', ['ionic', 'citizen-engagement.auth', 'citiz
             //State ISSUEDETAILS
             // This is the issue details state.
             .state('tab.issueDetailsList', {
-        // We use a parameterized route for this state.
-        // That way we'll know which issue to display the details of.
+// We use a parameterized route for this state.
+// That way we'll know which issue to display the details of.
         url: '/issueDetails/:issueId',
         views: {
-            // Here we use the same "tab-issueList" view as the previous state.
-            // This means that the issue details template will be displayed in the same tab as the issue list.
+// Here we use the same "tab-issueList" view as the previous state.
+// This means that the issue details template will be displayed in the same tab as the issue list.
             'tab-issueList': {
                 controller: 'GetIssueDetails',
                 templateUrl: 'templates/issueDetails.html'
@@ -117,12 +128,12 @@ angular.module('citizen-engagement', ['ionic', 'citizen-engagement.auth', 'citiz
         controller: "GetIssueDetails"
     })
             .state('tab.issueDetailsMap', {
-        // We use a parameterized route for this state.
-        // That way we'll know which issue to display the details of.
+// We use a parameterized route for this state.
+// That way we'll know which issue to display the details of.
         url: '/issueDetails/:issueId',
         views: {
-            // Here we use the same "tab-issueList" view as the previous state.
-            // This means that the issue details template will be displayed in the same tab as the issue list.
+// Here we use the same "tab-issueList" view as the previous state.
+// This means that the issue details template will be displayed in the same tab as the issue list.
             'tab-issueList': {
                 controller: 'GetIssueDetails',
                 templateUrl: 'templates/issueDetails.html'
@@ -132,7 +143,6 @@ angular.module('citizen-engagement', ['ionic', 'citizen-engagement.auth', 'citiz
     })
 
             ;
-
     // Define the default state (i.e. the first screen displayed when the app opens).
     $urlRouterProvider.otherwise(function($injector) {
         $injector.get('$state').go('tab.newIssue'); // Go to the new issue tab by default.
@@ -140,41 +150,40 @@ angular.module('citizen-engagement', ['ionic', 'citizen-engagement.auth', 'citiz
 })
         .run(function(AuthService, $rootScope, $state) {
 
-    // Listen for the $stateChangeStart event of AngularUI Router.
-    // This event indicates that we are transitioning to a new state.
-    // We have the possibility to cancel the transition in the callback function.
+// Listen for the $stateChangeStart event of AngularUI Router.
+// This event indicates that we are transitioning to a new state.
+// We have the possibility to cancel the transition in the callback function.
     $rootScope.$on('$stateChangeStart', function(event, toState) {
 
-        // If the user is not logged in and is trying to access another state than "login"...
+// If the user is not logged in and is trying to access another state than "login"...
         if (!AuthService.currentUserId && toState.name != 'login') {
 
-            // ... then cancel the transition and go to the "login" state instead.
+// ... then cancel the transition and go to the "login" state instead.
             event.preventDefault();
             $state.go('login');
         }
     });
 })
+
+
         .controller('LoginCtrl', function(apiUrl, AuthService, $http, $ionicHistory, $ionicLoading, $scope, $state) {
 
-    // The $ionicView.beforeEnter event happens every time the screen is displayed.
+// The $ionicView.beforeEnter event happens every time the screen is displayed.
     $scope.$on('$ionicView.beforeEnter', function() {
-        // Re-initialize the user object every time the screen is displayed.
-        // The first name and last name will be automatically filled from the form thanks to AngularJS's two-way binding.
+// Re-initialize the user object every time the screen is displayed.
+// The first name and last name will be automatically filled from the form thanks to AngularJS's two-way binding.
         $scope.user = {};
     });
-
     // Add the register function to the scope.
     $scope.register = function() {
 
-        // Forget the previous error (if any).
+// Forget the previous error (if any).
         delete $scope.error;
-
         // Show a loading message if the request takes too long.
         $ionicLoading.show({
             template: 'Logging in...',
             delay: 750
         });
-
         // Make the request to retrieve or create the user.
         $http({
             method: 'POST',
@@ -182,25 +191,21 @@ angular.module('citizen-engagement', ['ionic', 'citizen-engagement.auth', 'citiz
             data: $scope.user
         }).success(function(user) {
 
-            // If successful, give the user to the authentication service.
+// If successful, give the user to the authentication service.
             AuthService.setUser(user);
-
             // Hide the loading message.
             $ionicLoading.hide();
-
             // Set the next view as the root of the history.
             // Otherwise, the next screen will have a "back" arrow pointing back to the login screen.
             $ionicHistory.nextViewOptions({
                 disableBack: true,
                 historyRoot: true
             });
-
             // Go to the issue creation tab.
             $state.go('tab.newIssue');
-
         }).error(function() {
 
-            // If an error occurs, hide the loading message and show an error message.
+// If an error occurs, hide the loading message and show an error message.
             $ionicLoading.hide();
             $scope.error = 'Could not log in.';
         });
@@ -215,28 +220,54 @@ angular.module('citizen-engagement', ['ionic', 'citizen-engagement.auth', 'citiz
         .config(function($httpProvider) {
     $httpProvider.interceptors.push('AuthInterceptor');
 })
-
-        .controller("MapController", function($scope, mapboxMapId, mapboxAccessToken, geolocation) {
+        /*//////////////////////////////////////////MAPCONTROLLER//////////////////////////////////////////////*/
+        .controller("MapController", function($scope, mapboxMapId, mapboxAccessToken, Issues, geolocation) {
     var mapboxTileLayer = "http://api.tiles.mapbox.com/v4/" + mapboxMapId;
     mapboxTileLayer = mapboxTileLayer + "/{z}/{x}/{y}.png?access_token=" + mapboxAccessToken;
     $scope.mapDefaults = {
         tileLayer: mapboxTileLayer
     };
     $scope.mapCenter = {
-        lat: 51.48,
-        lng: 0,
+        lat: 46.7833,
+        lng: 6.65,
         zoom: 14
     };
     $scope.mapMarkers = [];
 
-    /*geolocation
-     geolocation.getLocation().then(function(data){
-     $scope.mapCenter.lat =data.coords.latitude;
-     $scope.mapCenter.lng =data.coords.longitude;
-     }, function(error) {
-     $log.error("Could not get location:" + error);
-     });      */
+    Issues.getIssues().then(function(data) {
+        $scope.issues = data.data;
+
+        for (var i = 0; i < $scope.issues.length; i++) {
+            var issue = $scope.issues[i];
+
+            var makeGetMessageScopeFunction = function(capturedIssue) {
+                return function() {
+                    var scope = $scope.$new();
+                    scope.issue = capturedIssue;
+                    return scope;
+                }
+            };
+            
+            $scope.mapMarkers.push({
+                lat: issue.lat,
+                lng: issue.lng,
+                message: "<p>{{ issue.id }}</p><img src='{{ issue.imageUrl }}' width='200px' />",
+                getMessageScope: makeGetMessageScopeFunction(issue)
+            });
+        }
+    });
+
 })
+
+
+/*geolocation
+ geolocation.getLocation().then(function(data){
+ $scope.mapCenter.lat =data.coords.latitude;
+ $scope.mapCenter.lng =data.coords.longitude;
+ }, function(error) {
+ $log.error("Could not get location:" + error);
+ });      */
+
 
         /*///////////////////////////////////GetIssueType///////////////////////////////////////////////*/
         .controller("GetIssueType", function($scope, $http, apiUrl) {
@@ -255,24 +286,24 @@ angular.module('citizen-engagement', ['ionic', 'citizen-engagement.auth', 'citiz
 
         .controller("IssuesListTabController", function($scope, $state) {
     $scope.onTabSelected = function() {
-        console.log("user has clicked on Issues List tab");
         $state.go('tab.issueList');
     }
 })
 
-        /*///////////////////////////////////GetIssues///////////////////////////////////////////////*/
-        .controller("GetIssues", function($scope, $http, apiUrl, $state, $ionicHistory) {
+        /*///////////////////////////////////ISSUELISTE///////////////////////////////////////////////*/
+        .controller("IssueList", function($scope, Issues, $state, $ionicHistory) {
+
+    var issues = Issues.getIssues();
     $scope.items = [];
-    $http({
-        method: 'GET',
-        url: apiUrl + '/issues'
-    })
-            .success(function(data) {
+
+    issues.success(function(data) {
         $scope.items = data;
     })
             .error(function(data) {
         console.log("Error");
     });
+
+
     $scope.goToDetails = function(issueId) {
         $state.go("tab.issueDetailsList", {issueId: issueId});
     }
@@ -299,7 +330,6 @@ angular.module('citizen-engagement', ['ionic', 'citizen-engagement.auth', 'citiz
             .error(function(data) {
         console.log("Error");
     });
-
 })
 
         /*///////////////////////////////////NewIssue///////////////////////////////////////////////*/
@@ -318,6 +348,5 @@ angular.module('citizen-engagement', ['ionic', 'citizen-engagement.auth', 'citiz
         console.log("Error");
     });
 })
-
 
 
--- www/js/auth.js
+++ www/js/auth.js
@@ -1,44 +1,42 @@
 angular.module('citizen-engagement.auth', ['angular-storage'])
 
-  .service('AuthService', function(store) {
+        .service('AuthService', function(store) {
 
     var service = {
-      currentUserId: store.get('currentUserId'),
-
-      setUser: function(user) {
-        service.currentUserId = user.userId;
-        store.set('currentUserId', user.userId);
-      },
-
-      unsetUser: function() {
-        service.currentUserId = null;
-        store.remove('currentUserId');
-      }
+        currentUserId: store.get('currentUserId'),
+        setUser: function(user) {
+            service.currentUserId = user.userId;
+            store.set('currentUserId', user.userId);
+        },
+        unsetUser: function() {
+            service.currentUserId = null;
+            store.remove('currentUserId');
+        }
     };
 
     return service;
-  })
+})
 
-  .factory('AuthInterceptor', function(AuthService) {
+        .factory('AuthInterceptor', function(AuthService) {
     return {
-      request: function(config) {
-        if (AuthService.currentUserId) {
-          config.headers['X-User-Id'] = AuthService.currentUserId;
+        request: function(config) {
+            if (AuthService.currentUserId) {
+                config.headers['X-User-Id'] = AuthService.currentUserId;
+            }
+
+            return config;
         }
-
-        return config;
-      }
     };
-  })
+})
 
-  .config(function($httpProvider) {
+        .config(function($httpProvider) {
     $httpProvider.interceptors.push('AuthInterceptor');
-  })
+})
 
-  .controller('LogoutCtrl', function(AuthService, $scope, $state) {
+        .controller('LogoutCtrl', function(AuthService, $scope, $state) {
     $scope.logOut = function() {
-      AuthService.unsetUser();
-      $state.go('login');
+        AuthService.unsetUser();
+        $state.go('login');
     };
-  })
-;
+})
+        ;
