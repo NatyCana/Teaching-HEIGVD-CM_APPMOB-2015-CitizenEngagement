d6b09fe12720df88776baece6113facf970667cf
NatyCana <naty@MBPdiNaCanonica.fritz.box>
2015-03-26 20:48:47 +0100

    modifications constants

--- www/js/constants.js
+++ www/js/constants.js
@@ -1,5 +1,5 @@
 angular.module('citizen-engagement.constants', [])
-  	.constant('apiUrl', 'http://localhost:8100/api-proxy')
+  	.constant('apiUrl', 'https://dry-oasis-5699.herokuapp.com/api')
 	.constant("mapboxMapId", "may0u.kajp3428")
 	.constant("mapboxAccessToken", "pk.eyJ1IjoibWF5MHUiLCJhIjoiejdhbFJaQSJ9.pLvm9eE9f02HkFvFOPPcCQ")
 ;
